84b8b065d413e564552ddf79881cefcb3d268f96
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-01-29 18:04:52 +0000

    created a function that iterates through each element with the class of 'calendar-row' and checks the value of the 'id' attribute of the child element with the class 'time-element' and color-coded each timeblock based on past, present, and future when the timeblock is viewed. The function is called once at the end of the code snippet.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,23 @@ $(document).ready(function(){
 	const currentDay = moment().format('dddd, MMMM Do YYYY');
 	$('#currentDay').html(currentDay);
 
+	function checkTime() {
+		let currentTime = moment().format('HH');
+		// console.log(currentTime);
+		$('.calendar-row').each(function(){
+			let timeBlock = $(this).find('.time-element').attr('id');
+			let textArea = $(this).find('.calendar-textarea');
+			if(currentTime === timeBlock) {
+				textArea.addClass('present').css('background-color','#adf7d1'); //green
+			} else if(currentTime < timeBlock){
+				textArea.addClass('future').css('background-color', '#f06868'); // yellow
+			} else {
+				textArea.addClass('past').css('background-color', '#606470'); //grey
+			}
+		});
+	}
 
-
-
+	checkTime();
 
 
 
